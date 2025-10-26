29e31f55ba3fb79267ad3be9fe669c41fef6b4cc
Srinath Narayanan <56746009+srinathn07@users.noreply.github.com>
2025-10-26 18:42:01 -0400

    Added ridership header to main.tsx

---
+++ src/app/ridership_and_system_performance.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold mb-4">
+        Ridership and System Performance
+      </h1>
+    </main>
+  );
+}
