c601a83176ebfa89bc4d384a380cf07fcbbe7380
Tyler Cartier <46423276+TylerCartier106@users.noreply.github.com>
2025-10-27 16:23:02 -0400

    Update page.tsx

--- src/app/departments/safety/page.tsx
+++ src/app/departments/safety/page.tsx
@@ -1,8 +1,12 @@
-export default function Safety() {
+import React from "react";
+
+const Safety: React.FC = () => {
     return (
         <div>
             <h1>Safety</h1>
             <p>This is the safety page here.</p>
         </div>
     );
-}
+};
+
+export default Safety;
