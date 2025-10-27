43c08de19dddc90cd7a29e17a728293ad382c3a4
Tyler Cartier <46423276+TylerCartier106@users.noreply.github.com>
2025-10-27 16:36:40 -0400

    Update page.tsx

--- src/app/departments/safety/page.tsx
+++ src/app/departments/safety/page.tsx
@@ -1,10 +1,12 @@
 import { JSX } from "react";
+import Link from "next/link";
 
 export default function Safety(): JSX.Element {
   return (
     <div>
       <h1>Safety</h1>
       <p>This is the safety page here.</p>
+      <Link href= "/">Home</Link>
     </div>
   );
 }
